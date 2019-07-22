0e9b8c98885c2b202fbc7b4fb141eb9f2229f64a
SergeiGray <borodat_for@mail.ru>
2019-07-22 15:24:24 +0300

    My proxy таблица

--- account/source/js/script.js
+++ account/source/js/script.js
@@ -2,7 +2,24 @@
 
 (function () {
 
-  let getShowInMyProxy = function () {
+  var hangSelect2 = function () {
+    $(document).ready(function() {
+      $('select').select2();
+    });
+  };
+
+  var scrollBarСustomization = function () {
+    $('body').niceScroll({
+      cursorcolor: "#CCCCCC",
+      cursorborder: "none"
+    });
+    $('tbody').niceScroll({
+      cursorcolor: "#CCCCCC",
+      cursorborder: "none"
+    });
+  }
+
+  var getShowInMyProxy = function () {
 
     var check = function (itemCheck, itemShow, itemLink) {
         $(itemCheck).prop('checked') ? $(itemShow).removeClass('display_none') && $(itemLink).addClass('my_proxy__link_active') : $(itemShow).addClass('display_none') && $(itemLink).removeClass('my_proxy__link_active');
@@ -41,6 +58,33 @@
     // });
   };
 
+  var getComentInMyProxy = function () {
+
+    $(window).on("load", function() {
+      $('.main__coment_text').each( function (index, el) {
+        var label = $(el).parents('td').find('.main__coment_label');
+        $(el)[0].scrollWidth > $(el).innerWidth() ? label.removeClass('my_proxy__coment_off') : label.addClass('my_proxy__coment_off');
+      });
+    });
+
+    $('.main__coment_text').focusout( function (evt) {
+      let text = $(evt.target).val();
+      let label = $(evt.target).parents('td').find('.main__coment_label');
+      label.text(text);
+      $(evt.target)[0].scrollWidth > $(evt.target).innerWidth() ? label.removeClass('my_proxy__coment_off') : label.addClass('my_proxy__coment_off');
+    });
+
+    $('.main__coment_text').mouseover( function (evt) {
+      let label = $(evt.target).parents('td').find('.main__coment_label');
+      let position = $(evt.target).offset();
+      let height = label.height();
+      label.attr('style', 'top: ' + (position.top - height - 25) + 'px;' + 'left: ' + (position.left - 50) + 'px;' );
+    });
+  };
+
+  hangSelect2();
+  scrollBarСustomization();
   getShowInMyProxy();
+  getComentInMyProxy();
 
 }());
