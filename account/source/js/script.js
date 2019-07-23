24c758bc61972ff85312ac029ef6f3a426be325d
SergeiGray <borodat_for@mail.ru>
2019-07-23 15:59:35 +0300

    Закончен блок my_proxy

--- account/source/js/script.js
+++ account/source/js/script.js
@@ -31,14 +31,14 @@
         $(itemCheck).prop('checked') ? $(itemShow).removeClass('display_none') && $(itemLink).addClass('my_proxy__link_active') : $(itemShow).addClass('display_none') && $(itemLink).removeClass('my_proxy__link_active');
     };
 
-    // var checkEdit = function (itemCheck, itemShow, itemLink) {
-    //     $(itemCheck).prop('checked') ? $(itemShow).removeClass('display_none') && $(itemLink).addClass('my_proxy__edit_link_active') : $(itemShow).addClass('display_none') && $(itemLink).removeClass('my_proxy__edit_link_active');
-    // };
+    var checkEdit = function (itemCheck, itemShow, itemLink) {
+        $(itemCheck).prop('checked') ? $(itemShow).removeClass('display_none') && $(itemLink).addClass('my_proxy__edit_link_active') : $(itemShow).addClass('display_none') && $(itemLink).removeClass('my_proxy__edit_link_active');
+    };
 
     $(".my_proxy__navigation .input").change( function() {
         check('#show__filter', '.my_proxy__filter', '.show__filter');
         check('#show__choose', '.my_proxy__choose', '.show__choose');
-        check('#show__change', '.my_proxy__change', '.show__change');
+        check('#show__edit', '.my_proxy__edit', '.show__edit');
         check('#show__auth', '.my_proxy__auth', '.show__auth');
         check('#show__export', '.my_proxy__export', '.show__export');
         check('#show__prolong', '.my_proxy__prolong', '.show__prolong');
@@ -50,18 +50,18 @@
         $('#show__none').trigger('click')
         check('#show__filter', '.my_proxy__filter', '.show__filter');
         check('#show__choose', '.my_proxy__choose', '.show__choose');
-        check('#show__change', '.my_proxy__change', '.show__change');
+        check('#show__edit', '.my_proxy__edit', '.show__edit');
         check('#show__auth', '.my_proxy__auth', '.show__auth');
         check('#show__export', '.my_proxy__export', '.show__export');
         check('#show__prolong', '.my_proxy__prolong', '.show__prolong');
       };
     });
 
-    // $(".my_proxy__edit_nav .main__input").change( function() {
-    //   checkEdit('#show__edit_login', '.my_proxy__edit_login', '.show__edit_login');
-    //   checkEdit('#show__edit_comment', '.my_proxy__edit_comment', '.show__edit_comment');
-    //   checkEdit('#show__edit_type', '.my_proxy__edit_type', '.show__edit_type');
-    // });
+    $(".my_proxy__edit_nav .main__input").change( function() {
+      checkEdit('#show__edit_login', '.my_proxy__edit_login', '.show__edit_login');
+      checkEdit('#show__edit_comment', '.my_proxy__edit_comment', '.show__edit_comment');
+      checkEdit('#show__edit_type', '.my_proxy__edit_type', '.show__edit_type');
+    });
   };
 
   var getComentInMyProxy = function () {
